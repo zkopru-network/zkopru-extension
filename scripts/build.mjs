590566eda88a443f5e23e9f0a7830118ff661e38
tkmct <1220t.takamichi@gmail.com>
2022-05-30 16:26:57 +0900

    refactor build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -14,29 +14,34 @@ const argv = yargs(process.argv.slice(2)).options({
     choices: ['all', 'firefox', 'chromium'],
     alias: 'target',
     default: 'all'
+  },
+  m: {
+    type: 'boolean',
+    alias: 'manifest',
+    default: 'true'
   }
 }).argv
 
 console.log(`Start building files for target: ${argv.t}`)
 
 if (argv.t === 'firefox') {
-  await buildFirefox()
+  await buildFirefox(argv)
 } else if (argv.t === 'chromium') {
-  await buildChromium()
+  await buildChromium(argv)
 } else if (argv.t === 'all') {
-  await buildFirefox()
-  await buildChromium()
+  await buildFirefox(argv)
+  await buildChromium(argv)
 } else {
   console.error('Invalid target')
   process.exit(1)
 }
 
-async function buildFirefox() {
-  console.log('Building files for Firefox')
-  const outdir = 'build/firefox'
-  const assetsDir = `${outdir}/assets`
+async function buildFiles(opt) {
+  const { m, outDir, manifestVersion } = opt
+  const assetsDir = `${outDir}/assets`
   if (!fs.existsSync(assetsDir)) fs.mkdirSync(assetsDir, { recursive: true })
 
+  console.log('building source files')
   // build popup file
   await build({
     entryPoints: {
@@ -44,7 +49,7 @@ async function buildFirefox() {
       contentscript: 'src/contentscript.ts',
       inpage: 'src/inpage.ts'
     },
-    outdir,
+    outdir: outDir,
     bundle: true,
     format: 'esm',
     plugins: [
@@ -62,75 +67,47 @@ async function buildFirefox() {
   // NOTE: background script is too large. need to reduce size or split files
   await build({
     entryPoints: ['src/background.ts'],
-    outdir,
+    outdir: outDir,
     bundle: true,
-    minify: true,
     format: 'esm',
     define: { window: 'globalThis' }
   })
 
   // copy html
-  fs.copyFileSync('src/ui/popup.html', `${outdir}/popup.html`)
+  fs.copyFileSync('src/ui/popup.html', `${outDir}/popup.html`)
 
   // copy manifest.json
-  // fs.copyFileSync('src/manifest_v3.json', 'build/manifest.json')
-  fs.copyFileSync('src/manifest/v2.json', `${outdir}/manifest.json`)
+  // TODO: switch manifest versions
+  if (m) {
+    console.log('generating manifest.json')
+    fs.copyFileSync(
+      `src/manifest/${manifestVersion}.json`,
+      `${outDir}/manifest.json`
+    )
+  }
 
   // copy icons
+  console.log('generating assets files')
   fs.copyFileSync('src/assets/icon-16.png', `${assetsDir}/icon-16.png`)
   fs.copyFileSync('src/assets/icon-32.png', `${assetsDir}/icon-32.png`)
 
-  console.log('Finish building files for Firefox')
+  console.log('Finish building files for', opt.targetName)
 }
 
-async function buildChromium() {
-  console.log('Building files for Chromium')
-  const outdir = 'build/chromium'
-  const assetsDir = `${outdir}/assets`
-  if (!fs.existsSync(assetsDir)) fs.mkdirSync(assetsDir, { recursive: true })
-
-  // build popup file
-  await build({
-    entryPoints: {
-      popup: 'src/ui/popup.tsx',
-      contentscript: 'src/contentscript.ts',
-      inpage: 'src/inpage.ts'
-    },
-    outdir,
-    bundle: true,
-    format: 'esm',
-    plugins: [
-      linaria.default({
-        sourceMap: true
-      })
-    ]
+async function buildFirefox(opt) {
+  await buildFiles({
+    ...opt,
+    outDir: 'build/firefox',
+    targetName: 'firefox',
+    manifestVersion: 'v2'
   })
+}
 
-  // build background script
-  // in background script context, window object is not defined.
-  // in order to build and use imported libraries built for web browser which depend on window global variable,
-  // it's necessary to replace window variable with globalThis
-  // FYI: https://esbuild.github.io/api/#define
-  // NOTE: background script is too large. need to reduce size or split files
-  await build({
-    entryPoints: ['src/background.ts'],
-    outdir,
-    bundle: true,
-    minify: true,
-    format: 'esm',
-    define: { window: 'globalThis' }
+async function buildChromium(opt) {
+  await buildFiles({
+    ...opt,
+    outDir: 'build/chromium',
+    targetName: 'chromium',
+    manifestVersion: 'v3'
   })
-
-  // copy html
-  fs.copyFileSync('src/ui/popup.html', `${outdir}/popup.html`)
-
-  // copy manifest.json
-  // fs.copyFileSync('src/manifest_v3.json', 'build/manifest.json')
-  fs.copyFileSync('src/manifest/v3.json', `${outdir}/manifest.json`)
-
-  // copy icons
-  fs.copyFileSync('src/assets/icon-16.png', `${assetsDir}/icon-16.png`)
-  fs.copyFileSync('src/assets/icon-32.png', `${assetsDir}/icon-32.png`)
-
-  console.log('Finish building files for Chromium')
 }
