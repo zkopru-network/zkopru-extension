fc4bbe2f412f2b9fb6b3e7aac41b1e82949d2cba
thebeyondr <jeopatto@gmail.com>
2022-07-01 18:43:44 -0500

    chore: :wrench: Update Storybook to use Tailwind (kind of)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: [
     '../src/ui/components/**/*.stories.mdx',
@@ -6,10 +8,38 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss')
+        }
+      }
+    }
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
   }
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\,css&/,
+  //     use: [
+  //       {
+  //         loader: 'postcss-loader',
+  //         options: {
+  //           postcssOptions: {
+  //             plugins: [require('tailwindcss'), require('autoprefixer')]
+  //           }
+  //         }
+  //       }
+  //     ],
+  //     include: path.resolve(__dirname, '../')
+  //   })
+  //   return config
+  // }
 }
