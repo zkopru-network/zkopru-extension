89514fceba7d633033a54fd57f9e41f21e5d07d9
thebeyondr <jeopatto@gmail.com>
2022-07-06 18:21:11 -0500

    feat: :lipstick: Update button component

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,46 +1,56 @@
 import React, { ReactNode } from 'react'
+import { Theme } from '../../interfaces'
 
-type Props = {
+interface BtnProps {
   children: ReactNode
   variant: string
-  size: string
+  theme: string
 }
 
-interface Variants {
-  primary: string
-  secondary: string
+interface BtnVariants {
+  primary: {
+    theme: Theme
+    getStyles: (theme: keyof Theme) => string
+  }
+  secondary: {
+    theme: Theme
+    getStyles: (theme: keyof Theme) => string
+  }
 }
 
-interface Sizes {
-  sm: string
-  md: string
-  lg: string
+const variants: BtnVariants = {
+  primary: {
+    theme: {
+      'sanctum-light': `bg-sanctum-blue/33 hover:bg-sanctum-blue`,
+      'sanctum-dark': 'bg-sanctum-blue-dark/33 hover:bg-sanctum-blue'
+    },
+    getStyles: function (theme: keyof Theme) {
+      return `transition font-medium text-base text-white rounded-full py-2 px-4 ${this.theme[theme]}`
+    }
+  },
+  secondary: {
+    theme: {
+      'sanctum-light':
+        'bg-sanctum-blue-light/33 border-2 border-sanctum-blue-light text-sanctum-blue-medium hover:bg-white',
+      'sanctum-dark':
+        'text-white border-2 border-sanctum-blue-medium/50 bg-white/10 hover:bg-sanctum-blue-medium'
+    },
+    getStyles: function (theme: keyof Theme) {
+      return `transition font-medium text-lg text-sm rounded-full py-1 px-2 ${this.theme[theme]}`
+    }
+  }
 }
 
-const defaulStyles =
-  'rounded-lg text-gray-900 relative transition font-medium text-lg'
-
-const variants: Variants = {
-  primary: `${defaulStyles} bg-blue-400 hover:bg-blue-500`,
-  secondary: `${defaulStyles} bg-gray-100 hover:bg-gray-200`
-}
-
-const sizes = {
-  sm: 'p-2 text-xs',
-  md: 'p-4 text-base',
-  lg: 'p-6'
-}
-
-const Button: React.FC<Props> = ({
-  children,
+const Button: React.FC<BtnProps> = ({
   variant = 'primary',
-  size = 'sm',
+  children = 'Click me',
+  theme = 'sanctum-light',
   ...rest
 }) => (
   <button
-    className={`${variants[variant as keyof Variants]} ${
-      sizes[size as keyof Sizes]
-    }`}
+    className={`${variants[variant as keyof BtnVariants].getStyles(
+      theme as keyof Theme
+    )}`}
     {...rest}
   >
     {children}
