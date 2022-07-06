310622e70e0b0ff81df41b7cd230dcfa8b803e3d
thebeyondr <jeopatto@gmail.com>
2022-07-06 18:18:54 -0500

    chore: :lipstick: Add font-family and theme colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // purge: [
-  //   '.src/ui/components/**/*.{js,ts,jsx,tsx}',
-  //   '.src/ui/pages/**/*.{js,ts,jsx,tsx}'
-  // ],
   mode: 'jit',
   content: [
     './src/ui/components/**/*.{js,ts,jsx,tsx}',
     './src/ui/pages/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'sanctum-blue': '#3030C6',
+        'sanctum-blue-dark': '#152059',
+        'sanctum-blue-light': '#CBD4FF',
+        'sanctum-blue-medium': '#3A405F'
+      },
+      opacity: {
+        33: '.33'
+      }
+    }
   },
   plugins: []
 }
