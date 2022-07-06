3f6accfefe09559131954f72aacf468b910985ba
thebeyondr <jeopatto@gmail.com>
2022-07-06 13:29:17 -0500

    refactor: :construction: Update button story

--- src/ui/components/Button/Button.stories.tsx
+++ src/ui/components/Button/Button.stories.tsx
@@ -4,12 +4,19 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 import Button from './Button'
 
 export default {
-  title: 'Buttons/Big Button',
-  component: Button
+  title: 'Big Button',
+  component: Button,
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: 'radio'
+    },
+    label: 'text'
+  }
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => (
-  <Button>My First Button</Button>
+  <Button>{args.label}</Button>
 )
 
 export const Primary = Template.bind({})
