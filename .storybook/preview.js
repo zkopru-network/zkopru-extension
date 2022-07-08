e861bd350762595ce70c637660554b3efaaadd07
thebeyondr <jeopatto@gmail.com>
2022-07-07 19:09:22 -0500

    chore: :wrench: Update storybook config to use themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,5 +7,20 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
+  },
+  themes: {
+    default: 'Sanctum',
+    list: [
+      {
+        name: 'Sanctum',
+        class: ['theme-sanctum', 'mode-light'],
+        color: '#8C62E7'
+      },
+      {
+        name: 'Sanctum Dark',
+        class: ['theme-sanctum', 'mode-dark'],
+        color: '#152059'
+      }
+    ]
   }
 }
