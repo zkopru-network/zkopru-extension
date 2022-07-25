e2e1630048e99aef8763e2765950c5cc76065c97
thebeyondr <jeopatto@gmail.com>
2022-07-25 17:42:25 -0500

    feat: :lipstick: Add alphabetical sorting to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -29,5 +29,11 @@ export const parameters = {
       { name: 'bg-sanctum-light', value: '#F9FAFF' },
       { name: 'bg-sanctum-dark', value: '#15172E' }
     ]
+  },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
   }
 }
