5aa7e0324a66897e6a16291e1f2522ccd2ad872f
thebeyondr <jeopatto@gmail.com>
2022-07-25 17:48:14 -0500

    refactor: :lipstick: Update variants and types for rounded buttons

--- src/ui/components/RoundedButton/index.tsx
+++ src/ui/components/RoundedButton/index.tsx
@@ -2,22 +2,21 @@ import React, { ReactNode } from 'react'
 
 interface BtnProps {
   children: ReactNode
-  variant: string
+  variant?: keyof RoundedBtnVariants
 }
 
 export interface RoundedBtnVariants {
-  primary: string
-  secondary: string
+  base: string
+  small: string
 }
 
 const variants: RoundedBtnVariants = {
-  // primary: `text-skin-primary bg-skin-light-gray hover:bg-btn-primary hover:text-white transition font-semibold rounded-md text-base rounded-4 py-4 px-6 flex focus:outline-none focus:ring focus:ring-btn-primary/50 focus:ring-offset-2`,
-  primary: `bg-btn-bright/33 hover:bg-btn-bright transition font-medium text-base text-white rounded-full py-2 px-4 focus:outline-none focus:ring focus:ring-btn-bright/50 focus:ring-offset-2`,
-  secondary: `primary-light bg-btn-bright/33 transition font-medium text-xs rounded-full py-1 px-2 focus:outline-none focus:ring focus:ring-btn-bright/75 focus:ring-offset-1 text-gray-100`
+  base: `bg-btn-bright/33 hover:bg-btn-bright transition font-medium text-base text-white rounded-full py-2 px-4 focus:outline-none focus:ring focus:ring-btn-bright/50 focus:ring-offset-2`,
+  small: `primary-light bg-btn-bright/33 transition font-medium text-xs rounded-full py-1 px-2 focus:outline-none focus:ring focus:ring-btn-bright/75 focus:ring-offset-1 text-gray-100`
 }
 
 const RoundedButton: React.FC<BtnProps> = ({
-  variant = 'primary',
+  variant = 'base',
   children = 'Click me'
 }) => (
   <button className={`${variants[variant as keyof RoundedBtnVariants]}`}>
