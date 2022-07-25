af7618bfab49d910b995aabd87f367840144450e
thebeyondr <jeopatto@gmail.com>
2022-07-25 17:45:29 -0500

    refactor: :building_construction: Better types for button variants

--- src/ui/components/Button/index.tsx
+++ src/ui/components/Button/index.tsx
@@ -6,24 +6,24 @@ interface BtnProps
     HTMLButtonElement
   > {
   children: ReactNode
-  variant: string
+  variant?: keyof BtnVariants
   icon?: JSX.Element
 }
 
 export interface BtnVariants {
-  primary: string
+  filled: string
   ghost: string
 }
 
 const commonButtonStyles = `transition disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-btn-bright/50 focus:ring-offset-2 focus:ring-offset-transparent font-semibold`
 
 const variants: BtnVariants = {
-  primary: `text-skin-text-primary bg-skin-light-gray hover:bg-btn-bright hover:text-white rounded-md py-4 px-6 ${commonButtonStyles}`,
+  filled: `text-skin-text-primary bg-skin-light-gray hover:bg-btn-bright hover:text-white rounded-md py-4 px-6 ${commonButtonStyles}`,
   ghost: `text-skin-text-primary border-skin-text-primary border-2 bg-transparent hover:border-btn-bright hover:bg-btn-bright hover:text-white rounded-md py-2 px-4 ${commonButtonStyles}`
 }
 
 const Button: React.FC<BtnProps> = ({
-  variant = 'primary',
+  variant = 'filled',
   children = 'New button',
   icon,
   ...rest
