2175dd863f0018a1ca9618609eae1bacd2579c0a
thebeyondr <jeopatto@gmail.com>
2022-08-02 18:42:15 -0500

    feat: :construction: Add Dashboard story + Extension header WIP

---
+++ src/ui/stories/Dashboard/Dashboard.stories.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/common/Header'
+import ExtensionFrame from '../../components/ExtensionFrame'
+
+export default {
+  title: 'Screens/Dashboard'
+}
+
+export const Dashboard = () => (
+  <ExtensionFrame>
+    <Header />
+    <h3>Dashboard</h3>
+    <h3>Lower</h3>
+  </ExtensionFrame>
+)
