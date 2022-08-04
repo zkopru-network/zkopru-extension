e152629164f787d075c876f2483c13633f60d66c
thebeyondr <jeopatto@gmail.com>
2022-08-04 13:58:47 -0500

    feat: :lipstick: Add submit input variant

--- src/ui/components/Input/Input.stories.tsx
+++ src/ui/components/Input/Input.stories.tsx
@@ -8,10 +8,13 @@ export default {
 } as ComponentMeta<typeof Input>
 
 export const Text: ComponentStory<typeof Input> = () => (
-  <Input label="Test label" id="testlabel"></Input>
+  <Input type="text" label="Test label" id="testlabel" />
 )
 
-export const Password: ComponentStory<typeof Input> = (args) => {
-  const { id, ...rest } = args
-  return <Input label="Password" id={id} type="password" {...rest}></Input>
+export const Password: ComponentStory<typeof Input> = () => {
+  return <Input type="password" label="Password" id={'password'} />
+}
+
+export const Submit: ComponentStory<typeof Input> = (args) => {
+  return <Input type="submit" label={args.label} id={'submit'} as={'ghost'} />
 }
--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
-
-interface InputProps
-  extends React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > {
-  label?: string
-  error?: string
-  type?: 'text' | 'password' | 'number'
-  id: string
-  extendClasses?: string
-}
+import { InputProps } from '../../interfaces'
+import { variants } from '../Button'
 
 const Input: React.FC<InputProps> = ({
   label,
@@ -18,27 +8,36 @@ const Input: React.FC<InputProps> = ({
   id,
   error,
   extendClasses: addClasses,
+  as,
   ...rest
-}) => (
-  <div className="flex flex-col gap-2 max-w-sm">
-    {label && (
-      <label htmlFor={id} className="text-sm">
-        {label}
-      </label>
-    )}
-    <input
-      className={`rounded-md bg-skin-light-gray text-skin-text-primary border border-skin-text-primary/33 shadow-sm focus:border-skin-text-primary focus:ring focus:ring-skin-text-primary focus:ring-opacity-60 invalid:border invalid:border-red-600/60 focus:invalid:border-red-600/75 focus:invalid:ring-red-500/75 peer ${addClasses}`}
-      type={type || 'text'}
-      name={id}
-      id={id}
-      {...rest}
-    />
-    {error && (
-      <p className="text-xs bg-red-600/60 text-red-100 p-1 pl-2 rounded-sm tracking-wide">
-        {error}
-      </p>
-    )}
-  </div>
-)
+}) => {
+  if (type === 'submit')
+    return <input type={type} className={variants[as]} value={label} />
+  else {
+    return (
+      <div className="flex flex-col gap-2 max-w-sm">
+        {label && (
+          <label htmlFor={id} className="text-sm">
+            {label}
+          </label>
+        )}
+
+        <input
+          className={`rounded-md bg-skin-light-gray text-skin-text-primary border border-skin-text-primary/33 shadow-sm focus:border-skin-text-primary focus:ring focus:ring-skin-text-primary focus:ring-opacity-60 invalid:border invalid:border-red-600/60 focus:invalid:border-red-600/75 focus:invalid:ring-red-500/75 peer ${addClasses}`}
+          type={type || 'text'}
+          name={id}
+          id={id}
+          {...rest}
+        />
+
+        {error && (
+          <p className="text-xs bg-red-600/60 text-red-100 p-1 pl-2 rounded-sm tracking-wide">
+            {error}
+          </p>
+        )}
+      </div>
+    )
+  }
+}
 
 export default Input
