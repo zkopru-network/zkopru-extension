56eda2259fdcaf7fbe893eac1c1f1204a7d5c811
thebeyondr <jeopatto@gmail.com>
2022-08-05 16:37:51 -0500

    fix: :construction: Update button styles

--- src/ui/components/RoundedButton/index.tsx
+++ src/ui/components/RoundedButton/index.tsx
@@ -4,8 +4,8 @@ import { BtnProps, RoundedBtnVariants } from '../../interfaces'
 const commonButtonStyles = `transition disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-btn-bright/50 focus:ring-offset-1 focus:ring-offset-white font-medium rounded-full border-2 border-skin-light-gray/25`
 
 const variants: RoundedBtnVariants = {
-  primary: `text-base py-2 px-4 text-white bg-btn-bright/75 hover:bg-btn-bright ${commonButtonStyles}`,
-  secondary: `text-xs py-1 px-2 text-skin-text-primary bg-skin-light-gray hover:bg-btn-bright hover:text-white ${commonButtonStyles}`
+  primary: `text-sm py-1 px-3 text-white bg-btn-bright/60 hover:bg-btn-bright ${commonButtonStyles}`,
+  secondary: `text-xs py-1 px-2 text-skin-light-gray bg-skin-light-gray/30 hover:bg-btn-bright hover:text-white ${commonButtonStyles}`
 }
 
 const RoundedButton: React.FC<BtnProps> = ({
