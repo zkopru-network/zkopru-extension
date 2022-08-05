cb6a420827766988fde69e93b68e181972d5f31c
thebeyondr <jeopatto@gmail.com>
2022-08-05 16:38:54 -0500

    fix: :lipstick: Update header

--- src/ui/components/common/Header.tsx
+++ src/ui/components/common/Header.tsx
@@ -4,12 +4,12 @@ import { ZkopruLogo } from './icons'
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center">
-      <ZkopruLogo />
-      <div className="flex gap-6 items-center justify-center">
-        <NetworkSwitcher />
-        <Settings />
+    <div className="flex justify-between items-center relative z-10">
+      <NetworkSwitcher />
+      <div className="absolute top-0 left-0 ml-48 mt-1">
+        <ZkopruLogo />
       </div>
+      <Settings />
     </div>
   )
 }
