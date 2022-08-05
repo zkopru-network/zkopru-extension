3888b9b05ced09512fbd685f579cbd11656bb826
thebeyondr <jeopatto@gmail.com>
2022-08-05 16:42:30 -0500

    refactor: :lipstick: Overview component now on Dashboard

--- src/ui/pages/Dashboard/index.tsx
+++ src/ui/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
 import Header from '../../components/common/Header'
 import ExtensionFrame from '../../components/ExtensionFrame'
+import { Overview } from '../../components/Overview'
 
 export const Dashboard = () => (
   <ExtensionFrame>
     <Header />
-    <h3>Dashboard</h3>
+    <Overview />
     <h3>Lower</h3>
   </ExtensionFrame>
 )
