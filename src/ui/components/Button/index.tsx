baa49455f2b12fea62b138c47a74cad580aa4783
thebeyondr <jeopatto@gmail.com>
2022-08-11 12:02:49 -0500

    feat: :lipstick: Update button component

--- src/ui/components/Button/index.tsx
+++ src/ui/components/Button/index.tsx
@@ -4,8 +4,8 @@ import { BtnProps, BtnVariants } from '../../interfaces'
 const commonButtonStyles = `transition disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-btn-bright/50 focus:ring-offset-2 focus:ring-offset-white/50 font-semibold`
 
 export const variants: BtnVariants = {
-  filled: `text-skin-text-primary bg-skin-light-gray hover:bg-btn-bright hover:text-white rounded-md py-1 px-2 ${commonButtonStyles}`,
-  ghost: `text-skin-text-primary border-skin-text-primary border-2 bg-transparent hover:border-btn-bright hover:bg-btn-bright hover:text-white rounded-md py-2 px-4 ${commonButtonStyles}`
+  filled: `text-skin-text-primary bg-skin-light-gray hover:bg-btn-bright hover:text-white rounded-md py-3 px-6 ${commonButtonStyles}`,
+  ghost: `text-skin-text-primary border-skin-text-primary border-2 bg-transparent hover:border-btn-bright hover:bg-btn-bright hover:text-white rounded-md py-3 px-6 ${commonButtonStyles}`
 }
 
 const Button: React.FC<BtnProps> = ({
@@ -15,7 +15,7 @@ const Button: React.FC<BtnProps> = ({
   ...rest
 }) => (
   <button {...rest} className={`${variants[variant as keyof BtnVariants]}`}>
-    <div className="flex gap-2 text-inherit justify-center items-center capitalize">
+    <div className="flex gap-2 text-inherit justify-center items-center">
       {children}
       {icon}
     </div>
