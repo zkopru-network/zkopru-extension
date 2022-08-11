d1874f72f983d5ac9e939ab373fbe615d8909b71
thebeyondr <jeopatto@gmail.com>
2022-08-11 12:01:34 -0500

    feat: :lipstick: Add header to extension frame

--- src/ui/components/ExtensionFrame.tsx
+++ src/ui/components/ExtensionFrame.tsx
@@ -1,11 +1,16 @@
+import Header from './common/Header'
+
 export default function ExtensionFrame({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col gap-8 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back">
-      {children}
+    <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back">
+      <>
+        <Header />
+        {children}
+      </>
     </div>
   )
 }
