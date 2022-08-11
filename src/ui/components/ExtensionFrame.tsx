50b7037a825fee69b10dc3ff76b0fdec424a634e
thebeyondr <jeopatto@gmail.com>
2022-08-11 16:16:27 -0500

    feat: :lipstick: Hide extension overflow

--- src/ui/components/ExtensionFrame.tsx
+++ src/ui/components/ExtensionFrame.tsx
@@ -6,7 +6,7 @@ export default function ExtensionFrame({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back">
+    <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back overflow-hidden">
       <>
         <Header />
         {children}
