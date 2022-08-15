c9893f9fe504f973fa8b309b46ecba4243c0838f
thebeyondr <jeopatto@gmail.com>
2022-08-15 16:47:02 -0500

    feat: :sparkles: Use react-hook-form in Onboarding

--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -1,43 +1,64 @@
 import React from 'react'
+import { Ref } from 'react'
 import { InputProps } from '../../interfaces'
 import { variants } from '../Button'
 
-const Input: React.FC<InputProps> = ({
-  label,
-  type,
-  id,
-  error,
-  extendClasses: addClasses,
-  as,
-  ...rest
-}) => {
-  if (type === 'submit')
-    return <input type={type} className={variants[as]} value={label} />
-  else {
-    return (
-      <div className="flex flex-col gap-2">
-        {label && (
-          <label htmlFor={id} className="text-sm">
-            {label}
-          </label>
-        )}
-
+const Input = React.forwardRef(
+  (
+    {
+      label,
+      type,
+      id,
+      error,
+      extendClasses: addClasses,
+      as,
+      ...rest
+    }: InputProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    if (type === 'submit')
+      return (
         <input
-          className={`rounded-md bg-skin-light-gray text-skin-text-primary border border-skin-text-primary/33 shadow-sm focus:border-skin-text-primary focus:ring focus:ring-skin-text-primary focus:ring-opacity-60 invalid:border invalid:border-red-600/60 focus:invalid:border-red-600/75 focus:invalid:ring-red-500/75 peer ${addClasses}`}
-          type={type || 'text'}
-          name={id}
-          id={id}
+          type={type}
+          className={variants[as]}
+          value={label}
+          ref={ref}
           {...rest}
         />
+      )
+    else {
+      return (
+        <div className="flex flex-col gap-[6px]">
+          {label && (
+            <label htmlFor={id} className="text-sm">
+              {label}
+            </label>
+          )}
 
-        {error && (
-          <p className="text-xs bg-red-600/60 text-red-100 p-1 pl-2 rounded-sm tracking-wide">
-            {error}
+          <input
+            className={`rounded-md bg-skin-light-gray text-skin-text-primary border-2 border-skin-text-primary/33 shadow-sm focus:border-skin-text-primary focus:ring focus:ring-skin-text-primary focus:ring-opacity-60 peer ${
+              error ? 'border-red-400/60' : ''
+            }  ${addClasses ? addClasses : ''}`}
+            type={type || 'text'}
+            name={id}
+            id={id}
+            {...rest}
+            ref={ref}
+          />
+
+          <p
+            className={`text-xs bg-red-600/60 text-red-100 p-1 pl-2 rounded-sm tracking-wide ${
+              error ? 'visible' : 'invisible'
+            }`}
+          >
+            {error ? `⚠️ ${error}` : 'Something went wrong'}
           </p>
-        )}
-      </div>
-    )
+        </div>
+      )
+    }
   }
-}
+)
+
+Input.displayName = 'Input'
 
 export default Input
--- src/ui/pages/Onboarding/index.tsx
+++ src/ui/pages/Onboarding/index.tsx
@@ -1,6 +1,9 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 import { ZkopruLogoWhite } from '../../components/common/icons'
 import ExtensionFrameWithImage from '../../components/ExtensionFrameWithImage'
-import { Password, Submit } from '../../components/Input/Input.stories'
+import Input from '../../components/Input'
 
 export const Onboarding = () => (
   <ExtensionFrameWithImage>
@@ -15,12 +18,66 @@ export const Onboarding = () => (
         manager, if you can.
       </p>
     </div>
-    <form>
-      <div className="flex flex-col gap-8">
-        <Password type="password" id="password" label="Password" />
-        <Password type="password" id="confirm" label="Confirm password" />
-        <Submit type="submit" label="Create account" as="ghost" id="register" />
-      </div>
-    </form>
+    <RegistrationForm />
   </ExtensionFrameWithImage>
 )
+
+const RegistrationForm = () => {
+  const validationSchema = z
+    .object({
+      password: z
+        .string()
+        .min(8, {
+          message: 'Password should be 8-64 characters'
+        })
+        .max(64, { message: 'Password should be 8-64 characters' }),
+      confirmPassword: z.string()
+    })
+    .refine((schema) => schema.password === schema.confirmPassword, {
+      message: 'Passwords should match',
+      path: ['confirmPassword']
+    })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(validationSchema)
+  })
+
+  return (
+    <form onSubmit={handleSubmit((d) => console.log(d))}>
+      <div className="flex flex-col gap-4">
+        <Input
+          type="password"
+          id="password"
+          label="Password"
+          error={
+            errors.password?.message
+              ? errors.password.message.toString()
+              : undefined
+          }
+          {...register('password')}
+        />
+        <Input
+          type="password"
+          id="confirm"
+          label="Confirm password"
+          {...register('confirmPassword')}
+          error={
+            errors.confirmPassword?.message
+              ? errors.confirmPassword.message.toString()
+              : undefined
+          }
+        />
+        <Input
+          type="submit"
+          label="Create account"
+          as="ghost"
+          id="registerBtn"
+        />
+      </div>
+    </form>
+  )
+}
