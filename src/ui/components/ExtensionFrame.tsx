e0b75c4eed6c3427b4044b84020125e7656f71ee
thebeyondr <beyondr42@gmail.com>
2022-08-19 11:54:07 -0500

    fix: :fire: Remove unnecessary Fragment in ExtensionFrame

--- src/ui/components/ExtensionFrame.tsx
+++ src/ui/components/ExtensionFrame.tsx
@@ -7,10 +7,8 @@ export default function ExtensionFrame({
 }) {
   return (
     <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back overflow-hidden">
-      <>
-        <Header />
-        {children}
-      </>
+      <Header />
+      {children}
     </div>
   )
 }
