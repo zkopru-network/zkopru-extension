fdda55db67dc5876f5e25cd82b5af09d204e5aca
thebeyondr <beyondr42@gmail.com>
2022-09-22 16:17:46 -0500

    refactor: :lipstick: Make ExtensionFrame relative

--- src/ui/components/ExtensionFrame.tsx
+++ src/ui/components/ExtensionFrame.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
+import { ReactNode } from 'react'
 import Header from './common/Header'
 
-export default function ExtensionFrame({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function ExtensionFrame({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back overflow-hidden">
+    <div className="flex flex-col gap-6 w-[464px] h-[614px] p-8 rounded-lg bg-skin-back overflow-hidden relative">
       <Header />
       {children}
     </div>
