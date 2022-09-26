a9cdba44df33822a6c21c41e4e0fa04df80ab38c
thebeyondr <beyondr42@gmail.com>
2022-09-26 16:59:05 -0500

    feat: :sparkles: Add `addClasses` attribute to buttons to extend styling

--- src/ui/interfaces.ts
+++ src/ui/interfaces.ts
@@ -119,6 +119,7 @@ export interface BtnProps
   children: ReactNode
   variant: keyof BtnVariants | keyof RoundedBtnVariants
   icon?: JSX.Element
+  addClasses?: string
 }
 
 export interface BtnVariants {
