80ac50ebd76451453fe5d29297197ed7d8c2d7a4
thebeyondr <beyondr42@gmail.com>
2022-09-28 14:22:03 -0500

    refactor: :adhesive_bandage: Tweak `ConfirmSwap` Playground copy

--- src/ui/pages/ConfirmSwap/index.tsx
+++ src/ui/pages/ConfirmSwap/index.tsx
@@ -21,7 +21,7 @@ const ConfirmSwapPage = () => {
           onClick={openModal}
           className="rounded-md bg-btn-bright px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-          Open dialog
+          Confirm swap
         </button>
       </div>
       <Modal
