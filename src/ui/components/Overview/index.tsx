0865c6707d87aae86f0172a28706a1c3b8194251
thebeyondr <beyondr42@gmail.com>
2022-09-28 14:59:25 -0500

    fix: :bug: Fix `Overview` component + story

--- src/ui/components/Overview/index.tsx
+++ src/ui/components/Overview/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import ROUTES from '../../../routes'
+import { ONBOARDING_URL } from '../../../share/constants'
 import {
   ArrowDownSFill,
   EyeLine,
@@ -7,39 +8,40 @@ import {
   ThreeDotsVertical
 } from '../common/icons'
 import RoundedButton from '../RoundedButton'
-import { ONBOARDING_URL } from '../../../share/constants'
-import ROUTES from '../../../routes'
 
 export const Overview = () => {
   const navigate = useNavigate()
 
-export const Overview = () => (
-  <div className="h-32 bg-gradient-to-br from-skin-g-primary to-skin-g-secondary rounded-3xl overflow-clip">
-    <div className="h-full flex flex-col justify-between bg-zk-logo bg-no-repeat bg-right p-5 text-white text-sm">
-      <section className="flex justify-between items-center">
-        {/* TODO: Maybe this is its own component? */}
-        <div className="flex gap-1 justify-center items-center relative">
-          <IconShield />
-          <p className="text-xs text-white" title="Account name">
-            Jörmungandr
-          </p>
-          <ArrowDownSFill className="text-white/60" />
-        </div>
-        <div className="flex gap-4 text-white items-center relative">
-          <p className="text-white text-xl font-medium" title="Current balance">
-            $2022.08
-          </p>
-          {/* TODO: Swap with other icon and trigger privacy */}
-          <EyeLine
-            className="peer hover:opacity-70 hover:cursor-pointer"
-            aria-describedby="privacy-tip"
-          />
-          {/* TODO: Make a tooltip component? */}
-          <div
-            className="absolute top-0 right-0 origin-top-right w-28 mr-16 invisible peer-hover:visible z-10 pl-2 py-[2px] text-xs bg-skin-back text-skin-text-primary rounded-lg flex justify-center items-center after:content-[''] after:-mr-[9px] after:border-[10px] after:border-t-transparent after:border-l-skin-back after:border-b-transparent after:border-r-transparent after:translate-x-1/2 transition"
-            role="tooltip"
-            id="privacy-tip"
->
+  return (
+    <div className="h-32 bg-gradient-to-br from-skin-g-primary to-skin-g-secondary rounded-3xl overflow-clip">
+      <div className="h-full flex flex-col justify-between bg-zk-logo bg-no-repeat bg-right p-5 text-white text-sm">
+        <section className="flex justify-between items-center">
+          {/* TODO: Maybe this is its own component? */}
+          <div className="flex gap-1 justify-center items-center relative">
+            <IconShield />
+            <p className="text-xs text-white" title="Account name">
+              Jörmungandr
+            </p>
+            <ArrowDownSFill className="text-white/60" />
+          </div>
+          <div className="flex gap-4 text-white items-center relative">
+            <p
+              className="text-white text-xl font-medium"
+              title="Current balance"
+            >
+              $2022.08
+            </p>
+            {/* TODO: Swap with other icon and trigger privacy */}
+            <EyeLine
+              className="peer hover:opacity-70"
+              aria-describedby="privacy-tip"
+            />
+            {/* TODO: Make a tooltip component? */}
+            <div
+              className="absolute top-0 right-0 origin-top-right w-28 mr-16 invisible peer-hover:visible z-10 pl-2 py-[2px] text-xs bg-skin-back text-skin-text-primary rounded-lg flex justify-center items-center after:content-[''] after:-mr-[9px] after:border-[10px] after:border-t-transparent after:border-l-skin-back after:border-b-transparent after:border-r-transparent after:translate-x-1/2 transition"
+              role="tooltip"
+              id="privacy-tip"
+            >
               Privacy mode
             </div>
             <ThreeDotsVertical />
@@ -69,16 +71,8 @@ export const Overview = () => (
               Withdraw
             </RoundedButton>
           </div>
-          <ThreeDotsVertical />
-        </div>
-      </section>
-      <section className="flex justify-between items-center">
-        <RoundedButton variant="secondary">Copy address</RoundedButton>
-        <div className="flex gap-2">
-          <RoundedButton variant="primary">Deposit</RoundedButton>
-          <RoundedButton variant="primary">Withdraw</RoundedButton>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   )
 }
